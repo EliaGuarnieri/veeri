9413ed038af1990481d342529f0de9135cd22cd7
Elia Guarnieri <elia.guarnieri@gmail.com>
2020-09-22 15:29:38 +0200

    Creata struttura base header

---
+++ components/TheHeader/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <header>
+    <div class="brand-name">
+      <nuxt-link to="/">HAPPY VUE YEAR</nuxt-link>
+    </div>
+
+    <div
+      class="drawer-toggle"
+      role="button"
+      @click="$store.dispatch('nav/toggleSidebar')"
+    >
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+
+    <div class="app-links">
+      <app-links></app-links>
+    </div>
+
+    <Sidebar />
+  </header>
+</template>
+
+<script>
+import AppLinks from './AppLinks'
+import Sidebar from './Sidebar'
+
+export default {
+  components: { AppLinks, Sidebar },
+  computed: {
+    isSidebar() {
+      return this.$store.getters['nav/toggleSidebar']
+    },
+  },
+  watch: {
+    $route() {
+      if (process.client && this.isSidebar && window.innerWidth < 768) {
+        this.$store.dispatch('nav/toggleSidebar')
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: grid;
+  grid-template: 60px / auto 1fr;
+  align-items: center;
+  background-color: #333;
+}
+
+.app-links {
+  justify-self: end;
+}
+
+.brand-name {
+  margin: 0 10px;
+  font-size: 1.3rem;
+}
+
+.brand-name a {
+  text-decoration: none;
+  color: white;
+}
+
+.drawer-toggle .bar {
+  width: 90%;
+  height: 2px;
+  background-color: white;
+}
+
+.drawer-toggle {
+  display: flex;
+  justify-self: end;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 50%;
+  width: 35px;
+  padding-right: 16px;
+  cursor: pointer;
+}
+
+@media (max-width: 767px) {
+  header {
+    padding: 0 16px;
+  }
+  header:nth-child {
+    justify-self: end !important;
+  }
+  .app-links {
+    display: none;
+  }
+}
+
+@media (min-width: 768px) {
+  header {
+    padding: 0 64px;
+  }
+  .app-links {
+    display: block;
+  }
+  .drawer-toggle {
+    display: none;
+  }
+}
+</style>

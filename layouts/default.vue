51a96d3de9fc70aae9f40a37b191e3a150378d82
Elia Guarnieri <elia.guarnieri@gmail.com>
2020-09-24 20:11:08 +0200

    Aggiunta transizione pagine.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,9 @@
     <TheHeader />
 
     <main>
-      <Nuxt />
+      <transition name="fade" mode="out-in">
+        <Nuxt />
+      </transition>
     </main>
   </div>
 </template>
@@ -20,5 +22,17 @@ export default {
 <style lang="scss" scoped>
 main {
   margin-top: $header-size;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.5s;
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>

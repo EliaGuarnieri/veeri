944486dec94fac6908edf9667bdbfdea2d3c99e2
Elia Guarnieri <elia.guarnieri@gmail.com>
2020-09-26 21:23:59 +0200

    Creato footer

---
+++ components/TheFooter/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Container :bg-color="variables.blu"> </Container>
+</template>
+
+<script>
+import Container from '@/components/Container'
+export default {
+  components: {
+    Container,
+  },
+  computed: {
+    variables() {
+      return this.$store.state.variables.styles
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>

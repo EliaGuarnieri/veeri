989e535762a85360e077edc3f86e0dfbb773542f
Elia Guarnieri <elia.guarnieri@gmail.com>
2020-09-26 21:23:38 +0200

    Creato container

---
+++ components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :style="{ 'background-color': bgColor }" class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgColor: {
+      type: String,
+      default: '#fff',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: $mobile-padding;
+
+  @include for-size(medium, from) {
+    padding: $desktop-padding;
+  }
+}
+</style>
